b4a13f4dfb54a69796b09e3b94f83acb6eafd704
Podter <48936015+Podter@users.noreply.github.com>
2022-12-31 19:08:19 +0700

    fixed the file path

--- update.sh
+++ update.sh
@@ -10,8 +10,10 @@ rm -rvf $BASE_DIR/repo/dists/anal/main/binary-amd64/**
 rm -rvf $BASE_DIR/repo/dists/anal/Release
 
 # make Packages and Packages.gz
-dpkg-scanpackages --arch amd64 $BASE_DIR/repo/pool/ > $BASE_DIR/repo/dists/anal/main/binary-amd64/Packages
+cd $BASE_DIR/repo
+dpkg-scanpackages --arch amd64 ./pool/ > $BASE_DIR/repo/dists/anal/main/binary-amd64/Packages
 cat $BASE_DIR/repo/dists/anal/main/binary-amd64/Packages | gzip -9 > $BASE_DIR/repo/dists/anal/main/binary-amd64/Packages.gz
 
 # make Release
-cd $BASE_DIR/repo/dists/anal/ && $BASE_DIR/scripts/generate-release.sh > Release
+cd $BASE_DIR/repo/dists/anal/
+$BASE_DIR/scripts/generate-release.sh > Release
